e0038e0d3f0341ce4ca46d980100fbed445bbeef
hepTsukubaKT <kotaro@hep.px.tsukuba.ac.jp>
2018-11-15 17:51:09 +0900

    諸々です

--- twitter/error.py
+++ twitter/error.py
@@ -9,16 +9,23 @@ M1  =   Symbol('M1');
 G0  =   Symbol('G0');
 G1  =   Symbol('G1');
 
+i=0
+u_I0=[]
+u_I1=[]
+
 #I0  =   (P0/M0)/((P0/M0)+(P1/M1))
 #I1  =   (P1/M1)/((P0/M0)+(P1/M1))
 
 I0     =G0/(G0+G1)
 I1     =G1/(G0+G1)
 #ファイル系
-data01  = np.loadtxt("./exp705.dat", comments='#')
-err     = np.loadtxt("./error/705_raw.txt", comments='#')
-pow_mrr = np.loadtxt("./mirror_pow/705_mrr.txt", comments='#')
+data01  = np.loadtxt("./exp476_tmp.dat", comments='#')
+err     = np.loadtxt("./error/476_raw.txt", comments='#')
+pow_mrr = np.loadtxt("./mirror_pow/476_mrr.txt", comments='#')
 
+# data01  = np.loadtxt("./exp705.dat", comments='#')
+# err     = np.loadtxt("./error/705_raw.txt", comments='#')
+# pow_mrr = np.loadtxt("./mirror_pow/705_mrr.txt", comments='#')
 
 
 #dI0_dxi =   [diff(I0,P0),diff(I0,P1),diff(I0,M0),diff(I0,M1)]
@@ -32,11 +39,10 @@ G0_num  =   data01[:,1]/data01[:,2]
 G1_num  =   data01[:,3]/data01[:,4]
 ui_0      =   (ui_raw / pow_mrr)  *   data01[:,1]
 ui_1      =   (ui_raw / pow_mrr)  *   data01[:,3]
+print(ui_0)
+print(ui_1)
 
 
-i=0
-u_I0=[]
-u_I1=[]
 # for i in range(5):
 #     u_I0.insert(i,  sqrt(   (dI0_dxi[0].subs([(P0,data01[i,1]),(P1,data01[i,3]),(M0,data01[i,2]),(M1,data01[i,4])])*(data01[i,1]*0.1))**2
 #                          +  (dI0_dxi[1].subs([(P0,data01[i,1]),(P1,data01[i,3]),(M0,data01[i,2]),(M1,data01[i,4])])*(data01[i,3]*0.1))**2
@@ -57,7 +63,7 @@ for i in range(5):
     #                     ))
     #
 #変更すること！
-np.savetxt("err705.dat",u_I0,delimiter='\n')
+np.savetxt("err476.dat",u_I0,delimiter='\n')
 #print(dI0_dxi)
 #print(dI1_dxi)
 print("error with 0-order")
